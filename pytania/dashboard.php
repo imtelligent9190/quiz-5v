e51765cf9944070c00f15c40232f40b39972596f
imtelligent9190 <91670009+imtelligent9190@users.noreply.github.com>
2022-02-10 16:13:33 +0100

    dashboard.php dodawanie pytan

--- pytania/dashboard.php
+++ pytania/dashboard.php
@@ -8,51 +8,126 @@ if (isset($_POST['id_quiz'])){
     unset($_POST['id_quiz']);
 }
 if(isset($_POST['submit_mul'])){
-    $questionNumber=$_POST['questionNumber'];
-    $questiontext=$_POST['questionText'];
-    $corectChoice=$_POST['correctChoice'];
+    $file = $_FILES['img'];
+    if($file["name"]){
+        
+        $fileTmpName = $_FILES['img']['tmp_name'];
+        $fileName = $_FILES["img"]["name"];
+        $fileExt = explode(".",$fileName);
+        $ext = strtolower(end($fileExt));
+        $fileName = uniqid("",true).".".$ext;
+        
+        $upload = '../foto/'.$fileName;
+    
+        move_uploaded_file($fileTmpName,$upload);
+    
+        $questionNumber=$_POST['questionNumber'];
+        $questiontext=$_POST['questionText'];
+        $corectChoice=$_POST['correctChoice'];
 
-    $choice=array();
-    $choice[1]=$_POST['choice1'];
-    $choice[2]=$_POST['choice2'];
-    $choice[3]=$_POST['choice3'];
-    $choice[4]=$_POST['choice4'];
-    $choice[5]=$_POST['choice5'];
+        $choice=array();
+        $choice[1]=$_POST['choice1'];
+        $choice[2]=$_POST['choice2'];
+        $choice[3]=$_POST['choice3'];
+        $choice[4]=$_POST['choice4'];
+        $choice[5]=$_POST['choice5'];
 
-    $sql="INSERT INTO `questions`(`id_quiz`, `QuestionNumber`, `QuestionText`, `img`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$questiontext."',null)";
+        $sql="INSERT INTO `questions`(`id_quiz`, `QuestionNumber`, `QuestionText`, `img`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$questiontext."','".$upload."')";
 
-    $insertrow=$mysqli->query($sql) or die($mysqli->error.__LINE__);
+        $insertrow=$mysqli->query($sql) or die($mysqli->error.__LINE__);
 
-    if($insertrow){
-        foreach ($choice as $choice => $value) {
-            if ($value!=''){
-                if($corectChoice==$choice){
-                    $isCorect=1;
-                }
-                else{
-                    $isCorect=0;
-                }
+        if($insertrow){
+            foreach ($choice as $choice => $value) {
+                if ($value!=''){
+                    if($corectChoice==$choice){
+                        $isCorect=1;
+                    }
+                    else{
+                        $isCorect=0;
+                    }
 
-                $sql2="INSERT INTO `choices`(`id_quiz`, `questionNumber`, `isCorrect`, `choiceText`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$isCorect."','$value')";
-                $inserrow2=$mysqli->query($sql2) or die($mysqli->error.__LINE__);
+                    $sql2="INSERT INTO `choices`(`id_quiz`, `questionNumber`, `isCorrect`, `choiceText`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$isCorect."','$value')";
+                    $inserrow2=$mysqli->query($sql2) or die($mysqli->error.__LINE__);
 
-                if($inserrow2){
-                    continue;
-                }
-                else{
-                    die ('Error: ('.$mysqli->errno.') '.$mysqli->error);
+                    if($inserrow2){
+                        continue;
+                    }
+                    else{
+                        die ('Error: ('.$mysqli->errno.') '.$mysqli->error);
+                    }
                 }
             }
+            $msg='Question has been added';
         }
-        $msg='Question has been added';
+    }
+    else{
+        $questionNumber=$_POST['questionNumber'];
+        $questiontext=$_POST['questionText'];
+        $corectChoice=$_POST['correctChoice'];
+
+        $choice=array();
+        $choice[1]=$_POST['choice1'];
+        $choice[2]=$_POST['choice2'];
+        $choice[3]=$_POST['choice3'];
+        $choice[4]=$_POST['choice4'];
+        $choice[5]=$_POST['choice5'];
+
+        $sql="INSERT INTO `questions`(`id_quiz`, `QuestionNumber`, `QuestionText`, `img`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$questiontext."',null)";
+
+        $insertrow=$mysqli->query($sql) or die($mysqli->error.__LINE__);
+
+        if($insertrow){
+            foreach ($choice as $choice => $value) {
+                if ($value!=''){
+                    if($corectChoice==$choice){
+                        $isCorect=1;
+                    }
+                    else{
+                        $isCorect=0;
+                    }
+
+                    $sql2="INSERT INTO `choices`(`id_quiz`, `questionNumber`, `isCorrect`, `choiceText`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$isCorect."','$value')";
+                    $inserrow2=$mysqli->query($sql2) or die($mysqli->error.__LINE__);
+
+                    if($inserrow2){
+                        continue;
+                    }
+                    else{
+                        die ('Error: ('.$mysqli->errno.') '.$mysqli->error);
+                    }
+                }
+            }
+            $msg='Question has been added';
+        }
     }
 }
 if(isset($_POST['submit_op'])){
-    $questionNumber=$_POST['questionNumber'];
-    $questiontext=$_POST['questionText'];
+    $file = $_FILES['img'];
+    if($file["name"]){
+        
+        $fileTmpName = $_FILES['img']['tmp_name'];
+        $fileName = $_FILES["img"]["name"];
+        $fileExt = explode(".",$fileName);
+        $ext = strtolower(end($fileExt));
+        $fileName = uniqid("",true).".".$ext;
+        
+        $upload = '../foto/'.$fileName;
+    
+        move_uploaded_file($fileTmpName,$upload);
+    
+        $questionNumber=$_POST['questionNumber'];
+        $questiontext=$_POST['questionText'];
 
-    $sql="INSERT INTO `questions`(`id_quiz`, `QuestionNumber`, `QuestionText`, `img`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$questiontext."',null)";
-    $insertrow=$mysqli->query($sql) or die($mysqli->error.__LINE__);
+        $sql="INSERT INTO `questions`(`id_quiz`, `QuestionNumber`, `QuestionText`, `img`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$questiontext."','".$upload."')";
+        $insertrow=$mysqli->query($sql) or die($mysqli->error.__LINE__);
+    }
+    else{
+        $questionNumber=$_POST['questionNumber'];
+        $questiontext=$_POST['questionText'];
+
+        $sql="INSERT INTO `questions`(`id_quiz`, `QuestionNumber`, `QuestionText`, `img`) VALUES('".$_SESSION['id']."','".$questionNumber."','".$questiontext."',null)";
+        $insertrow=$mysqli->query($sql) or die($mysqli->error.__LINE__);
+    }
 }
 $sql3 = "SELECT * FROM questions WHERE id_quiz='".$_SESSION['id']."'";
 $questions = $mysqli->query($sql3) or die($mysqli->error.__LINE__);
@@ -70,7 +145,7 @@ $next = $total+1;
 
         ?>
         <h3>Multiple Choice Questions</h3>
-        <form action="dashboard.php" method="POST">
+        <form action="dashboard.php" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="id-quiz" value=<?php echo $_SESSION['id'];  ?>>
             <p>
                 <label for="questionNumber">Question Number</label>
@@ -104,11 +179,15 @@ $next = $total+1;
                 <label for="correctChoice">Correct Choice Number:</label>
                 <input type="number" name="correctChoice">
             </p>
+            <p>
+            <label for="img" class="form-label" >Zdjęcie do Pytania</label>
+            <input id="img" class="form-control" type="file" name="img">
+            </p>
             <input type="submit" class="btn btn-primary" name="submit_mul" value="Submit">
             
         </form>
         <h3>Open-ended question</h3>
-        <form action="dashboard.php" method="POST">
+        <form action="dashboard.php" method="POST" enctype="multipart/form-data">
             <p>
                 <label for="questionNumber">Question Number</label>
                 <input type="number" value="<?php echo $next;?>" name="questionNumber">
@@ -116,6 +195,10 @@ $next = $total+1;
             <p>
                 <label for="questionText">Question Text</label>
                 <input type="text" name="questionText">
+            </p>
+            <p>
+            <label for="img" class="form-label" >Zdjęcie do Pytania</label>
+            <input id="img" class="form-control" type="file" name="img">
             </p>
             <input type="submit" class="btn btn-primary" name="submit_op" value="Submit">
         </form>
